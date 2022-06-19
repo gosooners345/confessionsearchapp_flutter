4e3a6cfb98cfc3b5c3deffd414f07ae3ca94beee
Brandon Guerin <Boomersooner12345@gmail.com>
2022-06-19 08:49:14 -0400

    Added files to the project related to different UI elements

---
+++ lib/splashscreen.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+
+class SplashScreenWidget extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState()  => SplashScreenWidgetState();
+
+
+}
+
+
+class SplashScreenWidgetState extends State<SplashScreenWidget> {
+
+  @override
+  void initState(){
+    super.initState();
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
