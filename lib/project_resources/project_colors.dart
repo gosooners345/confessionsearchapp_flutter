6fd923312908963605154f19af1f499e9311e245
GoSooners345 <boomersooner12345@gmail.com>
2022-06-22 12:28:08 -0400

    Start laying out the framework for the key parts of the application

---
+++ lib/project_resources/project_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class Styles {}
+
+class AppColors {
+  static Color mainAppColor = const Color(0xffDE031B);
+  static Color darkModeMain = const Color(0xffDE031B);
+}
---
+++ lib/user_interface_handling/notes_home_screen.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/material.dart';
+
+class NotesHomeWidget extends StatefulWidget{
+  @override
+State<NotesHomeWidget> createState() => _NotesHomeWidget();
+
+}
+
+class _NotesHomeWidget extends State<NotesHomeWidget>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
